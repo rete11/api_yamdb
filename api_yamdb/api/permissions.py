1c68deea5646a6cbcba91475150727b44c5d87ae
Denis Hrushev <super.hrushev2@mail.ru>
2023-12-04 15:52:07 +0300

    Fix permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,15 +2,19 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
-        return request.method in SAFE_METHODS or obj.author == request.user
+        return (request.method in SAFE_METHODS
+                or obj.author == request.user
+                or request.user.is_moderator
+                or request.user.is_admin)
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
-
-    def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.is_admin
 
 
@@ -18,18 +22,3 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (request.method in SAFE_METHODS
                 or (request.user.is_authenticated and request.user.is_admin))
-
-
-class IsOwner(BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        if not request.user.is_authenticated:
-            return False
-        return (obj.author == request.user
-                or request.user.is_moderator
-                or request.user.is_admin)
